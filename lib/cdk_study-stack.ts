db005f32180789aecd06fa9fdc86496f76a9c3ab
shuhei.morioka <selmertsx@gmail.com>
2020-07-16 15:59:24 +0900

    lambda function deploy用のsample code

--- lib/cdk_study-stack.ts
+++ lib/cdk_study-stack.ts
@@ -6,5 +6,12 @@ import { Duration } from '@aws-cdk/core';
 export class CdkStudyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new lambda.Function(this, "cdkStudyLambda", {
+      code: lambda.Code.asset("src/lambda"),
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_12_X,
+      timeout: Duration.seconds(20)
+    });
   }
 }
---
+++ src/lambda/index.ts
@@ -0,0 +1,7 @@
+export async function handler(event: any, context: any, callback: any) {
+  return {
+    statusCode: 200,
+    headers: {},
+    body: "ok"
+  };
+}
