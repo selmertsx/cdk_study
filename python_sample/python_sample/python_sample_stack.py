74f24657d3180b6d7266994a74f609046d74a86b
shuhei.morioka <selmertsx@gmail.com>
2020-07-23 22:46:11 +0900

    create cdk code

--- python_sample/python_sample/python_sample_stack.py
+++ python_sample/python_sample/python_sample_stack.py
@@ -1,9 +1,19 @@
-from aws_cdk import core
-
+from aws_cdk import (
+    core,
+    aws_lambda,
+    aws_apigateway
+)
 
 class PythonSampleStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        handler = aws_lambda.Function(
+            self,
+            "backend",
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            handler="handler.main",
+            code=aws_lambda.AssetCode(path="./lambda")
+        )
 
-        # The code that defines your stack goes here
+        aws_apigateway.LambdaRestApi(self, "SampleLambda", handler=handler)
