50905a492e9071a2048a1c5a2c5ed33afabc6623
shuhei.morioka <selmertsx@gmail.com>
2020-07-23 22:28:14 +0900

    create python lambda

---
+++ python_sample/lambda/handler.py
@@ -0,0 +1,12 @@
+import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def main(event, context):
+    logger.info(json.dumps(event))
+    return {
+        "statusCode": 200,
+        "body": "Hello World"
+    }
