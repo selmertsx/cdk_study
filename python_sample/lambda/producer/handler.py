6ba085056cba2c3d6e290c483ee848d363dfef79
shuhei.morioka <selmertsx@gmail.com>
2020-07-24 22:30:57 +0900

    create producer/handler

---
+++ python_sample/lambda/producer/handler.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+
+import json
+import uuid
+from decimal import Decimal
+import os
+import boto3
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource("dynamodb")
+TABLE_NAME = os.environ["TABLE_NAME"]
+
+def main(event, context):
+    table = dynamodb.Table(TABLE_NAME)
+    response = table.put_item(
+        Item={
+            "id": str(uuid.uuid4())
+        }
+    )
+    print("PutItem succeeded:")
+    print(json.dumps(response, indent=4, cls="DecimalEncoder"))
+    return { "statusCode": 200 }
